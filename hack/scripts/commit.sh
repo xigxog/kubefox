2b98ec27344fa06f4b067978ec82d1f082c3cdd1
John Long <xadhatter@pm.me>
2023-10-26 14:02:53 -0400

    update commit.sh to use def editor for commit msg

--- hack/scripts/commit.sh
+++ hack/scripts/commit.sh
@@ -4,10 +4,9 @@ source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
 
 $SCRIPTS/clean.sh
 $SCRIPTS/generate.sh
-$SCRIPTS/docs.sh
 
 go fmt ./...
 go vet ./...
 
 git add .
-git commit -m "${MSG}"
+git commit
