b312ba6ccac3ac449da04b25027a2b6f2ba93615
John Long <xadhatter@pm.me>
2023-12-12 12:17:20 -0500

    added kit sdk to docs

--- hack/scripts/docs.sh
+++ hack/scripts/docs.sh
@@ -3,13 +3,19 @@
 source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
 
 PROTOC_GEN_DOC_VERSION="v1.5.1"
+GOMARKDOC_VERSION="v1.1.0"
+
 FOX_ROOT="${REPO_ROOT}/../fox"
+KIT_DOCS_GO="${DOCS_SRC}/reference/kit/go"
 
 rm -rf ${DOCS_OUT}
 
 ### Install tools. If wrong version is installed, tool will be overwritten.
 ${TOOLS_DIR}/protoc-gen-doc -version | grep -q ${PROTOC_GEN_DOC_VERSION} ||
     go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@${PROTOC_GEN_DOC_VERSION}
+
+${TOOLS_DIR}/gomarkdoc --version | grep -q ${GOMARKDOC_VERSION} ||
+    go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@${GOMARKDOC_VERSION}
 ###
 
 # Generate docs from proto files.
@@ -26,6 +32,14 @@ if [ -d "${FOX_ROOT}" ]; then
     rm -rf ${DOCS_SRC}/reference/fox
     cp -r ${FOX_ROOT}/docs ${DOCS_SRC}/reference/fox
 fi
+
+# Generate docs from source files.
+rm -rf "${KIT_DOCS_GO}"
+mkdir -p "${KIT_DOCS_GO}"
+
+find "${REPO_ROOT}/kit" -type d -exec bash -c '
+gomarkdoc --output "${0}/$(basename "${1}").md" "${1}"
+' "${KIT_DOCS_GO}" {} \;
 
 pipenv install
 pipenv run mkdocs build
