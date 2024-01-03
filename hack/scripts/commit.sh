1402d62eaaa13c6052d21b2b1564730538ddae26
John Long <xadhatter@pm.me>
2024-01-03 10:56:19 -0500

    added license header to src files

--- hack/scripts/commit.sh
+++ hack/scripts/commit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Copyright 2023 XigXog
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
 
 source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
 
@@ -12,6 +19,7 @@ mkdir -p ${TOOLS_DIR}
 go install github.com/google/addlicense@v1.1.1
 ${TOOLS_DIR}/addlicense -f license.tpl \
     -l mpl -c XigXog -y 2023 \
+    -ignore ".markdownlint.yaml" \
     -ignore ".github/**" \
     -ignore "examples/**" \
     -ignore "site/**" \
