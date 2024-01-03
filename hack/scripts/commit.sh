68f17fb1463c1a908a1a8fbfc133aa77653ee9ec
John Long <xadhatter@pm.me>
2024-01-03 10:44:55 -0500

    added addlicense to commit.sh

--- hack/scripts/commit.sh
+++ hack/scripts/commit.sh
@@ -6,6 +6,18 @@ ${SCRIPTS}/clean.sh
 ${SCRIPTS}/generate.sh
 ${SCRIPTS}/docs.sh
 
+mkdir -p ${TOOLS_DIR}
+
+# Ensure all source files have copyright header.
+go install github.com/google/addlicense@v1.1.1
+${TOOLS_DIR}/addlicense -f license.tpl \
+    -l mpl -c XigXog -y 2023 \
+    -ignore ".github/**" \
+    -ignore "examples/**" \
+    -ignore "site/**" \
+    -ignore "workstation/**" \
+    .
+
 go mod tidy
 gofmt -l -s -w .
 go vet ./...
